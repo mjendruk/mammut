1c2604a2eefd06dfc69505bab92d08b9e6e5f744
Carolin Fiedler <carolin.fiedler@student.hpi.uni-potsdam.de>
2013-11-21 14:12:54 +0100

    remove unnecessary physics code

--- source/GameLogic.cpp
+++ source/GameLogic.cpp
@@ -39,30 +39,8 @@ void GameLogic::initialize()
 
     glm::mat4 mat = glm::translate(0.f, 10.0f, 0.0f);
 
-    m_cuboids << new Cuboid(m_dynamicsWorld, glm::vec3(.3, .5f, 2.f));
-    m_cuboids << new Cuboid(m_dynamicsWorld, glm::vec3(.5f, .5f, 1.f), glm::vec3(0.f, 10.0f, 0.0f));
-
-    ////btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-
-    //btCollisionShape * fallShape = new btBoxShape(btVector3(.5f, .5f, 1.f));
-
-    //btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-
-    ////btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-    ////btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
-
-    ////m_dynamicsWorld->addRigidBody(groundRigidBody);
-
-    ///*btDefaultMotionState* fallMotionState =
-    //    new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 10, 0)));*/
-
-    ////btScalar mass = 1;
-    ////btVector3 fallInertia(0, 0, 0);
-    ////fallShape->calculateLocalInertia(mass, fallInertia);
-
-    ////btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
-    //m_fallRigidBody = new btRigidBody(fallRigidBodyCI);
-    //m_dynamicsWorld->addRigidBody(m_fallRigidBody);
+    m_cuboids << new Cuboid(m_dynamicsWorld, glm::vec3(.3, .5f, 2.f), glm::vec3(0.5f, 0.0f, 0.0f));
+    m_cuboids << new Cuboid(m_dynamicsWorld, glm::vec3(.5f, .5f, 1.f), glm::vec3(0.f, 1.0f, 0.0f));
 }
 
 void GameLogic::update(int ms)
