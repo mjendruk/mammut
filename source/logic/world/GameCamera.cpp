5e56f09ce4e507fe03d6de45485369850db44659
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-02-04 21:34:14 +0100

    mammut doesn't rotate on gravity switch but camera does

--- source/logic/world/GameCamera.cpp
+++ source/logic/world/GameCamera.cpp
@@ -9,10 +9,10 @@
 #include "Mammut.h"
 
 const glm::vec3 GameCamera::s_centerOffset = glm::vec3(0.0f, 0.0f, -1.0f);
-const float GameCamera::rotationDuration = 0.25f;
+const glm::vec3 GameCamera::s_eyeOffset = glm::vec3(0.0f, 0.05f, 0.0f);
+const float GameCamera::s_rotationDuration = 0.25f;
 
-GameCamera::GameCamera(Mammut & mammut)
-:   m_mammut(mammut)
+GameCamera::GameCamera()
 {
 }
 
@@ -20,43 +20,43 @@ GameCamera::~GameCamera()
 {
 }
 
-void GameCamera::update(float seconds)
+void GameCamera::update(const glm::vec3 & position, float seconds)
 {
-	if (m_lastRotation != m_mammut.rotation()) {
-		m_rotationProgress = 0.0;
-		m_lastRotation = m_mammut.rotation();
-		m_oldRotation = m_currentRotation;
-	}
-
-	m_rotationProgress = std::min(m_rotationProgress + seconds, rotationDuration);
-
-	m_currentRotation = calculateRotation(m_oldRotation, m_mammut.rotation(), m_rotationProgress);
-
-	m_center = m_mammut.position() + s_centerOffset;
-    
-    const glm::vec4 homogenousEyeOffset = m_currentRotation * glm::vec4(0.0f, 0.05f, 0.0f, 1.0f);
-	m_eye = m_mammut.position() + homogenousEyeOffset.xyz() / homogenousEyeOffset.w;
-    
-    const glm::vec4 homogenousUp = m_currentRotation * glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
-	m_up = homogenousUp.xyz() / homogenousUp.w;
+	updateRotationProgress(seconds);
+    updateLookAt(position);
 }
 
-const glm::mat4 GameCamera::calculateRotation(const glm::mat4 & oldRotation, const glm::mat4 & targetRotation, float rotationProgress)
+void GameCamera::gravityChangeEvent(const glm::mat3 & rotation)
 {
-	//convert to angles
-	float oldAngle = glm::degrees(atan2(oldRotation[0][1], oldRotation[0][0]));
-	float targetAngle = glm::degrees(atan2(targetRotation[0][1], targetRotation[0][0]));
+    m_initialRotation = m_currentRotation;
+    m_targetRotation = rotation;
+    m_rotationProgress = 0.0f;
+}
 
-	//prevent 270° turns
-	if (targetAngle - oldAngle > 181)
-		oldAngle += 360;
-	if (oldAngle - targetAngle > 181)
-		targetAngle += 360;
+void GameCamera::updateRotationProgress(float seconds)
+{
+    m_rotationProgress = std::min(m_rotationProgress + seconds, s_rotationDuration);
+    
+	// convert to angles
+	const float initialAngle = glm::degrees(atan2(m_initialRotation[0][1], m_initialRotation[0][0]));
+	const float targetAngle = glm::degrees(atan2(m_targetRotation[0][1], m_targetRotation[0][0]));
 
-	//convert to quaternions and slerp them
-	glm::quat from = glm::rotate(glm::quat(), oldAngle, glm::vec3(0.0, 0.0, 1.0));
+	// convert to quaternions and slerp them
+	glm::quat from = glm::rotate(glm::quat(), initialAngle, glm::vec3(0.0, 0.0, 1.0));
 	glm::quat to = glm::rotate(glm::quat(), targetAngle, glm::vec3(0.0, 0.0, 1.0));
-	return glm::mat4_cast(glm::mix(from, to, glm::smoothstep(0.0f, 1.0f, rotationProgress / rotationDuration)));
+	
+    m_currentRotation = glm::mat3_cast(glm::slerp(from, to, glm::smoothstep(0.0f, 1.0f, m_rotationProgress / s_rotationDuration)));
+}
+
+void GameCamera::updateLookAt(const glm::vec3 & position)
+{
+    m_center = position + s_centerOffset;
+    
+    const glm::vec4 homogenousEyeOffset = glm::mat4(m_currentRotation) * glm::vec4(s_eyeOffset, 1.0);
+	m_eye = position + homogenousEyeOffset.xyz() / homogenousEyeOffset.w;
+    
+    const glm::vec4 homogenousUp = glm::mat4(m_currentRotation) * glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
+	m_up = homogenousUp.xyz() / homogenousUp.w;
 }
 
 const glm::vec3 & GameCamera::eye() const
--- source/logic/world/GameCamera.h
+++ source/logic/world/GameCamera.h
@@ -1,38 +1,38 @@
 #pragma once
 
+#include <QObject>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-class Mammut;
-
-class GameCamera
+class GameCamera : public QObject
 {
 public:
-    GameCamera(Mammut & mammut);
-
+    GameCamera();
     virtual ~GameCamera();
 
-    void update(float seconds);
-
+    void update(const glm::vec3 & position, float seconds);
+    void gravityChangeEvent(const glm::mat3 & rotation);
 
     const glm::vec3 & eye() const;
     const glm::vec3 & center() const;
     const glm::vec3 & up() const;
 
 protected:
-	static const glm::mat4 calculateRotation(const glm::mat4 & oldRotation, const glm::mat4 & targetRotation, float rotationProgress);
+    void updateRotationProgress(float seconds);
+    void updateLookAt(const glm::vec3 & position);
 
     static const glm::vec3 s_centerOffset;
-	static const float rotationDuration;
+    static const glm::vec3 s_eyeOffset;
+	static const float s_rotationDuration;
 
     glm::vec3 m_eye;
     glm::vec3 m_center;
     glm::vec3 m_up;
 
-	glm::mat4 m_lastRotation;
-	glm::mat4 m_oldRotation;
-	glm::mat4 m_currentRotation;
+    glm::mat3 m_initialRotation;
+    glm::mat3 m_targetRotation;
+	glm::mat3 m_currentRotation;
 	float m_rotationProgress;
 
-    Mammut & m_mammut;
 };
