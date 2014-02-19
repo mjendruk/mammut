f7d534a4ecbf9d231649127f05790af39abaef2f
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2014-02-19 01:20:17 +0100

    enable back face culling note: glCullFace(GL_BACK); glFrontFace(GL_CCW); are system independent OpenGL defaults

--- source/rendering/world/GameWorldRenderer.cpp
+++ source/rendering/world/GameWorldRenderer.cpp
@@ -66,6 +66,7 @@ void GameWorldRenderer::drawGeometry()
     PerfCounter::begin("geom");
     m_gBufferFBO->bind();
     
+    glEnable(GL_CULL_FACE);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     // set the default view space depth to - s_farPlane
