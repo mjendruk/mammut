2b26ee9e1414de6cdab1c697ddbcba11b44ecd7a
Carolin Fiedler <carolin.fiedler@student.hpi.uni-potsdam.de>
2014-03-16 19:21:53 +0100

    start position of mammut on cuboid

--- source/logic/world/GameMechanics.cpp
+++ source/logic/world/GameMechanics.cpp
@@ -10,8 +10,8 @@
 const float GameMechanics::s_caveRadius = 150.0f;
 
 GameMechanics::GameMechanics()
-,   m_mammut(glm::vec3(0.f, 7.6f, 0.0f))
 :   m_chunkGenerator(std::chrono::system_clock::now().time_since_epoch().count())
+,   m_mammut(glm::vec3(0.f, 5.05f, 0.0f))
 ,   m_gameOver(false)
 ,   m_backgroundLoop(Sound::kLoop, true)
 {
